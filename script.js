a1064342f5bb791a81de5633493f8503d83d09f6
Asif Sarker <gbasifsarker078@gmail.com>
2024-11-13 06:10:35 +0600

    Add basic calculator functionality to SmartCalc Pro

--- script.js
+++ script.js
@@ -1,18 +1,94 @@
-function appendToResult(value) {
-    document.getElementById('result').value += value;
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
 }
 
-function clearResult() {
-    document.getElementById('result').value = '';
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperator(parseInt(buffer));
+            previousOperator = null;
+            buffer = `${runningTotal}`;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+    }
 }
 
-function deleteCharacter() {
-    var result = document.getElementById('result').value;
-    document.getElementById('result').value = result.slice(0,-1)
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperator(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
 }
 
-function calculateResult() {
-    var result = document.getElementById('result').value;
-    var calculation = eval(result);
-    document.getElementById('result').value = calculation;
+function flushOperator(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else if (previousOperator === '÷') {
+        if (intBuffer !== 0) {
+            runningTotal /= intBuffer;
+        } else {
+            buffer = "Error";
+        }
+    }
 }
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function (event) {
+        buttonClick(event.target.innerText.trim());
+    });
+}
+
+init();
